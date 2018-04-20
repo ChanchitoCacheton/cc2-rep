8daa586787b1527d61affcf8f8633a08fe0bcde2
Jean Paul Ari <uboa@MacBook-Pro-de-Jean.local>
2018-04-20 08:18:21 -0500

    ej1_sem5

---
+++ sem5_ej1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void Print_arr(int *e,int *arr){
+
+    cout<<"[ ";
+    for(int i=0;i<sizeof(arr);i++){
+        cout<< *arr;
+
+    }
+    cout<<"]";
+
+
+
+}
+
+
+
+int main(){
+
+
+
+    return 0;
+}
