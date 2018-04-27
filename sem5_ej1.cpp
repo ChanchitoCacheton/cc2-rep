92e4022afbbcb5e56e9267e1bcfaa3b392ac8020
Jean Paul Ari <uboa@MacBook-Pro-de-Jean.local>
2018-04-27 07:34:03 -0500

    semana5 v1

--- sem5_ej1.cpp
+++ sem5_ej1.cpp
@@ -2,17 +2,64 @@
 
 using namespace std;
 
+
+void sort_Cocktail(int a[], int n){
+
+    bool estaCambiado = true;
+    int inicio = 0;
+    int fin = n - 1;
+ 
+    while (estaCambiado) {
+        // reset the swapped flag on entering
+        // the loop, because it might be true from
+        // a previous iteration.
+        estaCambiado = false;
+ 
+        // loop from left to right same as
+        // the bubble sort
+        for (int i = inicio; i < fin; ++i) {
+            if (a[i] > a[i + 1]) {
+                swap(a[i], a[i + 1]);
+                estaCambiado = true;
+            }
+        }
+ 
+        // if nothing moved, then array is sorted.
+        if (!estaCambiado)
+            break;
+ 
+        // otherwise, reset the swapped flag so that it
+        // can be used in the next stage
+        estaCambiado = false;
+ 
+        // move the end point back by one, because
+        // item at the end is in its rightful spot
+        --fin;
+ 
+        // from right to left, doing the
+        // same comparison as in the previous stage
+        for (int i = fin - 1; i >= inicio; --i) {
+            if (a[i] > a[i + 1]) {
+                swap(a[i], a[i + 1]);
+                estaCambiado = true;
+            }
+        }
+ 
+        // increase the starting point, because
+        // the last stage would have moved the next
+        // smallest number to its rightful spot.
+        ++inicio;
+    }
+}
+
+
 void Print_arr(int *e,int *arr){
 
     cout<<"[ ";
-    for(int i=0;i<sizeof(arr);i++){
+    for(int i=0;i<sizeof(arr);arr++){
         cout<< *arr;
-
     }
     cout<<"]";
-
-
-
 }
 
 
